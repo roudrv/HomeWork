9ac00d65744309c4961c476c93a4c2c3210c9082
Popok Ruslan <rvpopok@gmail.com>
2023-04-06 16:34:14 +0300

    add HomeWork8

--- HomeWork8/src/task2/Main.java
+++ HomeWork8/src/task2/Main.java
@@ -7,10 +7,12 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
         Integer[] array = {1, 2, 4, 4};
-        System.out.println(arrayToMap(array));
+        //System.out.println(arrayToMap(array));
+        printMap(arrayToMap(array));
 
         String[] array1 = {"Один", "Два", "Два", "Два", "Три"};
-        System.out.println(arrayToMap(array1));
+        //System.out.println(arrayToMap(array1));
+        printMap(arrayToMap(array1));
     }
 
     public static <K> Map<K, Integer> arrayToMap(K[] ks) {
@@ -25,5 +27,19 @@ public class Main {
             map.put(val, count);
         }
         return map;
+    }
+
+    public static <K> void  printMap(Map<K, Integer> map){
+        System.out.print("{");
+        int count = 0;
+        for(Map.Entry<K, Integer> val: map.entrySet()){
+            if(count != 0){
+                System.out.print(", " + val.getKey() + ":" + val.getValue());
+            }else{
+                System.out.print(val.getKey() + ":" + val.getValue());
+            }
+            count++;
+        }
+        System.out.println("}");
     }
 }
